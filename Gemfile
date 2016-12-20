164571c6aca1a6a2bd33fcfb8600ca72eb4b48c9
Pea M. Tyczynska <CrystalPea@github.com>
2016-12-20 15:21:04 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+
+source "https://rubygems.org"
+ruby '2.3.1'
+
+group :test do
+  gem "rspec"
+  gem "rspec-sinatra"
+  gem "dm-rspec"
+  gem "capybara"
+  gem "database_cleaner"
+  gem "pry"
+end
+
+gem "sinatra"
+gem "dm-postrges-adapter"
+gem "data_mapper"
+gem "bcrypt"
+gem "rake"
+gem "sinatra-flash"
+gem "rack"
---
+++ spec/features/admin_sign_up_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.feature "Sign up as admin" :type => :feature do
+  scenario "user signs up" do
+  end
+
+  scenario "user uses incorrect email format" do
+  end
+
+  scenario "user uses taken username" do
+  end
+
+  scenario "user uses taken email" do
+  end
+
+  scenario "password confirmation does not match password" do
+  end
+
+  scenario "user uses forbidden special characters" do
+  end
+end
