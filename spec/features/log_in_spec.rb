1b84d2edfc060a6023a8f2a0418385214411cd59
Pea M. Tyczynska <CrystalPea@github.com>
2016-12-26 17:13:10 +0100

    Log in feature - first test written

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+RSpec.feature "Log in" do
+  include Helpers
+
+  let(:user_1) do {
+    name: "Pea",
+    username: "pea",
+    email: "peaczek@gmail.com",
+    password: "Mikocian"
+    }
+  end
+
+  scenario "I would like to log in" do
+    sign_up(user_1)
+    click_link("Log out")
+    click_link("Log in")
+    expect(current_path).to eq "/sessions/new"
+    fill_in "username", with: "pea"
+    fill_in "password", with: "Mikocian"
+    click_button "Log in"
+    expect(current_path).to eq "/dashboard"
+    message = "Welcome to Creative Playlist, Pea!"
+    expect(page).to have content(message)
+  end
+
+end
